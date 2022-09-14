8ae2c910cc1082e75bfde6b2e6b74db45a8b7269
elharitekamal <abdelkhalekelharite@gmail.com>
2022-09-14 09:04:22 +0100

    coding

---
+++ Triangle Etoile/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j, o, L;
+    L=10;
+
+for(o=0; o<L; o++){
+    for(j=0; j<L-o; j++)
+        printf(" ");
+    for(i=0; i<=2*o; i++){
+        printf("*");
+    }
+    printf("\n");
+    }
+  
+
+    return 0;
+}
